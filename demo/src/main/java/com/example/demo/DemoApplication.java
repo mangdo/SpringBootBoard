f29f063f0f1ee10b3a4e1eefbd87012dd796da3b
mangdo <70243735+mangdo@users.noreply.github.com>
2021-04-09 22:55:24 +0900

    test code

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		//main 메소드에서 실행하는 SpringApplication.run으로 인해 내장 WAS를 실행한다.
 	}
 
 }
